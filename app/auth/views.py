08adbe0242c03768d82c7b2884195a78ea9c3237
feysal-Ibrahim <addictivefazman@gmail.com>
2018-09-16 12:38:45 +0300

    add logout view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -32,8 +32,3 @@ def register():
     return render_template ( 'auth/register.html' , registration_form=form )
 
 
-@auth.route ( '/logout' )
-@login_required
-def logout():
-    logout_user ( )
-    return redirect ( url_for ( "main.index" ) )
