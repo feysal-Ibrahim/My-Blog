02408ad610b601147936f61e95273f387d799ee3
feysal-Ibrahim <addictivefazman@gmail.com>
2018-09-17 11:04:57 +0300

    create form for  post

--- config.py
+++ config.py
@@ -1,16 +1,21 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://feisal:1234@localhost/blog'
-    SECRET_KEY = '123'
+    SECRET_KEY = os.environ.get ( 'SECRET_KEY' )
 
-
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get ( "MAIL_USERNAME" )
+    MAIL_PASSWORD = os.environ.get ( "MAIL_PASSWORD" )
 
 
 class ProdConfig(Config):
-    pass
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get ( "DATABASE_URL" )
 
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://feisal:1234@localhost/pitch'
 
     DEBUG = True
 
